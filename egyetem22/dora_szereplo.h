d1205005100f271691ae0a4f3b8b8686ecd29379
shilamila <dori92@freemail.hu>
2022-04-02 10:35:48 +0200

    :)

--- egyetem22/dora_szereplo.h
+++ egyetem22/dora_szereplo.h
@@ -1,5 +1,15 @@
 #pragma once
-class dora_szereplo
+#include "szereplo.h"
+class dora_szereplo :public szereplo
 {
+public:
+	dora_szereplo(int x, int y) : szereplo(x, y, 'D') {
+		
+	}
+	virtual void szamit() {
+		this->x = x + 3;
+		if (x > 70)x = 0;
+		this->y = y--;
+		if (y < 1) y = 50;
+	}
 };
-
