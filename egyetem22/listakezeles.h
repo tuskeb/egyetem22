89f8ceed856487c6817a4cc29155f55741d1b951
tuskeb <tuskebganz@gmail.com>
2022-04-02 12:32:26 +0200

    Listakezelés

--- egyetem22/listakezeles.h
+++ egyetem22/listakezeles.h
@@ -1,24 +1,68 @@
 #pragma once
+#include <iostream>
 //Tanár Úr
 template <typename T>
 class listakezeles
 {
 	private:
 		T* adatok = NULL;
-		int mmeret = 0;
-
+		int m_meret = 0;
+		int m_elemszam = 0;
 	public:
-		DinamikusTomb(int meret) {
-			this->mmeret = meret;
+		listakezeles(int meret) {
+			this->m_meret = meret;
 			adatok = new T[meret];
 		}
-		~DinamikusTomb() {
+
+		~listakezeles() {
 			delete adatok;
 		}
+
 		T& operator[](const int index) {
 			return adatok[index];
 		}
-		const int& meret() {
-			return mmeret;
+
+		int hozzaad(T adat) {
+			if (m_elemszam < m_meret)
+			{
+				adatok[m_elemszam] = adat;
+				m_elemszam++;
+			}
+			else {
+				//cout << "Megtelt a tömb";
+				return -1;
+			}
 		}
+
+		T eltavolit(int index) {
+			T r = adatok[index];
+			for (int i = index; i < m_meret - 1; i++) {
+				adatok[i] = adatok[i + 1];
+			}
+			adatok[m_meret-1] = NULL;
+			m_elemszam--;
+			return r;
+		}
+
+		
+		int eltavolitertek(T ertek) {
+			int db = 0;
+			for (int i = 0; i < m_elemszam; i++) {
+				if (adatok[i] == ertek) {
+					eltavolit(i);
+					db++;
+				}
+			}
+			return db;
+		}
+		
+
+		const int& meret() {
+			return m_meret;
+		}
+
+		const int& elemszam() {
+			return m_elemszam;
+		}
+
 };
--- egyetem22/main.cpp
+++ egyetem22/main.cpp
@@ -11,37 +11,33 @@
 #include "dani_szereplo.h"
 #include "gabor_szereplo.h"
 #include "zsolt_szereplo.h"
+#include "listakezeles.h"
 
 using namespace std;
 
 int main() {
-	int db = 10;
 
-	szereplo** szereplok = new szereplo*[db];
-	for (int i = 0; i < db; i++) {
-		szereplok[i] = NULL;
-		//cout << *szereplok[i] << endl;
-	}
-	szereplok[0] = new tibor_szereplo(2,5);
-	szereplok[1] = new tanarur_szereplo(5, 5);
-	szereplok[2] = new kristof_szereplo(10, 5);
-	szereplok[3] = new dora_szereplo(15, 5);
-	szereplok[4] = new dani_szereplo(20, 5);
-	szereplok[5] = new zsolt_szereplo(25, 5);
-	szereplok[6] = new gabor_szereplo(30, 5);
+	szereplo* t;
+	listakezeles<szereplo*> szereplok(200);
+	szereplok.hozzaad(new tibor_szereplo(2, 5));
+	szereplok.hozzaad(t = new tanarur_szereplo(5, 5));
+	szereplok.hozzaad(new kristof_szereplo(10, 5));
+	szereplok.hozzaad(new dora_szereplo(15, 5));
+	szereplok.hozzaad(new dani_szereplo(20, 5));
+	szereplok.hozzaad(new zsolt_szereplo(25, 5));
+	szereplok.hozzaad(new gabor_szereplo(30, 5));
 
-	while(!GetKeyState(VK_F10)) {
-		for (int i = 0; i < db; i++) {
-			if (szereplok[i] != NULL) {
-				szereplok[i]->rajzol();
-			}
+	szereplok.eltavolitertek(t);
+
+	while (!GetKeyState(VK_F10)) {
+		for (int i = 0; i < szereplok.elemszam(); i++) {
+			szereplok[i]->rajzol();
 		}
 		std::this_thread::sleep_for(160ms);
 	}
-	
-	for (int i = 0; i < db; i++) {
-		delete szereplok[i];
+
+	while (szereplok.elemszam() > 0) {
+		delete szereplok.eltavolit(0);
 	}
-	delete szereplok;
 
 }
