d1ce900c1c515cc459494f8bc0720b3685e07739
nemethkristof18 <nemethkristof18@gmail.com>
2022-04-02 10:48:44 +0200

    Kristof

--- egyetem22/kristof_szereplo.cpp
+++ egyetem22/kristof_szereplo.cpp
@@ -1,12 +1,1 @@
-#pragma once
-#include "szereplo.h"
-class kristof_szereplo : public szereplo
-{
-	virtual void szamit() {
-		this->y++;
-		if (y > 50) {
-			y = 0;
-		}
-	}
-};
-
+#include "kristof_szereplo.h"
