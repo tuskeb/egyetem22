90b77d9812707035d55d8d7b3cb2d1f26b884d3f
shilamila <dori92@freemail.hu>
2022-04-02 12:38:46 +0200

    :)

--- egyetem22/dora_szereplo.h
+++ egyetem22/dora_szereplo.h
@@ -1,15 +1,25 @@
 #pragma once
-#include "szereplo.h"
+#include "szereplo.h" // csiga
+#include <time.h>
+#include <stdio.h>    
+#include <stdlib.h>
 class dora_szereplo :public szereplo
 {
 public:
 	dora_szereplo(int x, int y) : szereplo(x, y, 'D') {
-		
 	}
 	virtual void szamit() {
-		this->x = x + 3;
-		if (x > 30)x = 0;
-		this->y = y--;
-		if (y < 1) y = 40;
+		if (GetAsyncKeyState(VK_RIGHT)) {
+			x = x++;
+		}
+		if (GetAsyncKeyState(VK_DOWN)) {
+			y = y++;
+		}
+		if (GetAsyncKeyState(VK_UP)) {
+			y = y--;
+		}
+		if (GetAsyncKeyState(VK_LEFT)) {
+			x = x--;
+		}
 	}
 };
