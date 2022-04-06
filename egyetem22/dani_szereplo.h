4f66b3a7dc8f3e05d601723919c444ce2dbbf2b1
Dániel Talabér <nuada7@gmail.com>
2022-04-06 08:36:39 +0200

    homework_0

--- egyetem22/dani_szereplo.h
+++ egyetem22/dani_szereplo.h
@@ -1,15 +1,21 @@
 #pragma once
 #include "szereplo.h"
+#include "display.h"
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
 class dani_szereplo: public szereplo
 {
 public:
 	dani_szereplo(int x, int y) : szereplo(x, y, 'O') {
 
 	}
+	dani_szereplo(int y) : szereplo(1, y, 'O') {
+	}
 
 	virtual void szamit() {
-		this->x++;
-		if (x > 19) { x = 1; }
+		if (x < 70) { this->x = x++; }
 	}
+	virtual bool utveg() { return x == 70; }
 };
 
